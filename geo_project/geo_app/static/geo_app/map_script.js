6928e423bab5b2164cf790564df9a872ce3a7448
pxie1985 <xphn1985@hotmail.com>
2023-10-03 10:11:47 -0500

    first success in publishing via js in static folder

--- geo_project/geo_app/static/geo_app/map_script.js
+++ geo_project/geo_app/static/geo_app/map_script.js
@@ -1,31 +1,20 @@
-console.log("JavaScript file is loaded!");
+ require([
+        "esri/Map",
+        "esri/views/MapView",
+        "esri/layers/MapImageLayer"
+    ], function (Map, MapView, MapImageLayer) {
 
-require([
-    "esri/Map",
-    "esri/views/MapView",
-    "esri/layers/MapImageLayer"
-], function (Map, MapView, MapImageLayer) {
+        var layer = new MapImageLayer({
+            url: "https://geohwp.houstontx.gov/arcgis/rest/services/01_BaseData_Infrastructure/Lift_Stations/MapServer"
+        });
 
-    var layer = new MapImageLayer({
-        url: "https://geohwp.houstontx.gov/arcgis/rest/services/01_BaseData_Infrastructure/Force_Mains/MapServer"
+        var map = new Map({
+            layers: [layer]
+        });
+
+        var view = new MapView({
+            container: "viewDiv",
+            map: map
+        });
 
     });
-
-    console.log("Layer created:", layer);  // Log after creating the layer
-
-    var map = new Map({
-        layers: [layer]
-    });
-
-
-    console.log("Map created:", map);  // Log after creating the map
-
-
-    var view = new MapView({
-        container: "viewDiv",
-        zoom: 10,
-        center: [-95.3698, 29.7604]
-    });
-    console.log("View created:", view);  // Log after creating the view
-
-});
